c81f9437edd026030c10564677a7d214285afaa4
zaifrun <martinknud@gmail.com>
2021-06-17 14:04:54 +0200

    updated gradle

--- app/src/main/java/org/pondar/spinnerviewdemokotlin/MainActivity.kt
+++ app/src/main/java/org/pondar/spinnerviewdemokotlin/MainActivity.kt
@@ -6,16 +6,20 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import org.pondar.spinnerviewdemokotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     //you can also put these values in xml - the book shows how.
     private val items = arrayOf("Item 0", "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7")
 
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         //The spinner is defined in our xml file
 
@@ -25,9 +29,9 @@ class MainActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this,
                 android.R.layout.simple_spinner_dropdown_item, items)
 
-        spinner1.adapter = adapter
+        binding.spinner1.adapter = adapter
 
-        spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        binding.spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 
             //The AdapterView<?> type means that this can be any type,
             //so we can use both AdapterView<String> or any other
@@ -41,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onNothingSelected(arg0: AdapterView<*>) {
                 // you would normally do something here
-                // for instace setting the selected item to "null"
+                // for instance setting the selected item to "null"
                 // or something.
             }
         }
@@ -51,8 +55,8 @@ class MainActivity : AppCompatActivity() {
         val adapter2 = ArrayAdapter.createFromResource(
                 this, R.array.spinner_array, android.R.layout.simple_spinner_dropdown_item)
 
-        spinner2.adapter = adapter2
-        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        binding.spinner2.adapter = adapter2
+        binding.spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 //String test = getResources().getString(R.string.hello_world);
                 val listArray = resources.getStringArray(R.array.spinner_array)
@@ -69,10 +73,10 @@ class MainActivity : AppCompatActivity() {
         //This line of code can always be used to get the
         //selected position in in the spinner - the first item
         //will have an index of 0.
-        val position = spinner1.selectedItemPosition
+        val position = binding.spinner1.selectedItemPosition
 
-        //This line will get the actual seleted item -
+        //This line will get the actual selected item -
 
-        val item = spinner1.selectedItem
+        val item = binding.spinner1.selectedItem
     }
 }
